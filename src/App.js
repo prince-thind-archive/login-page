bde8e26a877f4c9632c849792cb22c33464dd4c5
Prince Thind <Princethind799@gmail.com>
2022-02-08 11:55:33 +0530

    design header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import "antd/dist/antd.min.css";
+import { Button, PageHeader, Typography } from "antd";
+
+import { cyan, orange } from "@ant-design/colors";
+
+const Title = Typography.Title;
+const primaryColor = cyan[8];
+const secondaryColor = orange.primary;
 
 function App() {
+  const title = (
+    <Title level={3} style={{ color: primaryColor }}>
+      ATools<span style={{ color: secondaryColor }}>.</span>
+    </Title>
+  );
+
   return (
     <div className="App">
-     React App
+      <PageHeader
+        title={title}
+        extra={[
+          <Button
+            style={{
+              backgroundColor: primaryColor,
+              color: "white",
+              padding: "0 2rem",
+            }}
+          >
+            Start Free Trial
+          </Button>,
+          <Button
+            style={{
+              backgroundColor: secondaryColor,
+              color: "white",
+              padding: "0 2rem",
+            }}
+          >
+            Login
+          </Button>,
+        ]}
+      ></PageHeader>
     </div>
   );
 }
